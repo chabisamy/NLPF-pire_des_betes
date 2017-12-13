3287ccb1b84a147924ddcc7ebb01ecc8fbb4d772
Hamza <hamzasamy.chabi@gmail.com>
2017-12-13 19:54:19 +0100

    up

---
+++ Django/mysite/mysite/urls.py
@@ -0,0 +1,33 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from mysite.core import views as core_views
+from mysite.projects import views as project_views
+from django.contrib.auth import views as auth_views
+
+
+
+urlpatterns = [
+    url(r'^$', project_views.index, name='index'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
+    url(r'^new_project/$', project_views.new_project, name='new_project'),
+    url(r'^project/(\d{1,})/$', project_views.project, name='project'),
+    url(r'^participate/(\d{1,})/$', project_views.participate, name='participate'),
+    url(r'^signup/$', core_views.signup, name='signup'),
+]
--- README.md
+++
@@ -1,5 +0,0 @@
-# NLPF-pire_des_betes
-## Le retour : Python Django
-
-Objectif:
-- [ ] Afficher une page web
